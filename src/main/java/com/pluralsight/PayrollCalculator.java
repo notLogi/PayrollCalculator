ff9e78cd5136f64e89456711daa92ef5bb7651fe
notLogi <92322066+notLogi@users.noreply.github.com>
2025-09-25 11:49:00 -0400

    Payroll Calculator

--- src/main/java/com/pluralsight/PayrollCalculator.java
+++ src/main/java/com/pluralsight/PayrollCalculator.java
@@ -5,11 +5,11 @@ public class PayrollCalculator {
     public static void main(String[] args) {
         Scanner myScanner = new Scanner(System.in);
 
-        System.out.println("Enter your name: ");
+        System.out.print("Enter your name: ");
         String myName = myScanner.nextLine();
-        System.out.println("Enter your hours worked: ");
+        System.out.print("Enter your hours worked: ");
         float hoursWorked = myScanner.nextFloat();
-        System.out.println("What is your pay rate? ");
+        System.out.print("What is your pay rate? ");
         float payRate = myScanner.nextFloat();
         float grossPay = hoursWorked * payRate;
         System.out.println("Employee's name: " + myName);
